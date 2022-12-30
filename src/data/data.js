80792f68d40ef6d2757cd3ee70c173d8c94729eb
JailbreakerVC <vijit123chandna@gmail.com>
2022-12-30 12:55:42 +0530

    updates from dell

--- src/data/data.js
+++ src/data/data.js
@@ -1,28 +1,39 @@
 export default [
      {
-          tags: ["thoughts"],
+          id: 1,
+          tag: 'stress',
           name: "Focusing on reality",
           description: "I will focus on reality and not on my thoughts",
-          task: "Get yourself a glass on tea/coffee whatever, Drink it as slowly as possible, savour the taste and notice the temperature, observe it move down your throat, whenever you feel like you are getting hooked by your thoughts - Focus on the drink!"
+          task: "Get yourself a drink/beverage, Drink it as slowly as possible, savour the taste and notice the temperature, observe it move down your throat, whenever you feel like you are getting hooked by your thoughts - Focus on the drink!"
      },
      {
-          tags: ["fghfgh"],
-          name: "fucking off",
-          description: "I will focus on reality and not on my thoughts",
-          task: "fuck off"
+          id:2,
+          tag:'gratitude',
+          name: "I love my coffee",
+          description: "nill",
+          task:"sit back and think of the things that you’re thankful for, give it time and thought, it can be anything as simple as your warm coffee or the beautiful sunrise."
      },
      {
-          tags: ["tfgh"],
-          name: "Focusing on reality",
-          description: "I will focus on reality and not on my thoughts",
-          task: " down your throat"
+          id:3,
+          tag:'stress',
+          name: "Ground yourself",
+          description: "nill",
+          task:"Notice how you are feeling and what you are thinking...then slow down and connect with your body, breathe out (slowly) stretch (slowly) and push your feet into the ground...refocus on the world around you, notice where you are and what you are doing"
      },
      {
-          tags: ["tfghs"],
-          name: "Focusing on reality",
-          description: "I will focus on reality and not on my thoughts",
-          task: "Get/coffee whatever, Drink it as slowly as possible, savour the taste and notice the temperature, observe it move down your throat"
-     },
+          id:4,
+          tag:'thoughts and feelings',
+          name: "Write it down",
+          description: "nill",
+          task:"Notice your difficult thoughts and feelings, name the thoughts and feelings (silently) and refocus on what you are doing and tell yourself what is important to you",
      
+     },
+     {
+          id:5,
+          tag:'thoughts and feelings',
+          name: "Write it down",
+          description: "nill",
+          task:""
+     }
 
 ]
