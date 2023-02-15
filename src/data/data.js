2022211f2ccb61bbd6cd820fc300280db3ae1216
JailbreakerVC <vijit123chandna@gmail.com>
2023-02-15 09:54:23 +0530

    fixed some tasks

--- src/data/data.js
+++ src/data/data.js
@@ -10,21 +10,21 @@ export default [
           id:2,
           tag:'gratitude',
           name: "I love my coffee",
-          description: "nill",
+          description: "focusing on what matters",
           task:"sit back and think of the things that you’re thankful for, give it time and thought, it can be anything as simple as your warm coffee or the beautiful sunrise."
      },
      {
           id:3,
           tag:'stress',
           name: "Ground yourself",
-          description: "nill",
+          description: "letting your feelings be",
           task:"Notice how you are feeling and what you are thinking...then slow down and connect with your body, breathe out (slowly) stretch (slowly) and push your feet into the ground...refocus on the world around you, notice where you are and what you are doing"
      },
      {
           id:4,
           tag:'thoughts and feelings',
           name: "Write it down",
-          description: "nill",
+          description: "letting your feelings be",
           task:"Notice your difficult thoughts and feelings, name the thoughts and feelings (silently) and refocus on what you are doing and tell yourself what is important to you",
      
      },
